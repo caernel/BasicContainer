7854debb7dcaf1d8ad12c66635b61971a47054ba
rmello4d <ricardo.mello@4d.com>
2013-10-17 19:41:28 +0200

    Identation

--- widget.js
+++ widget.js
@@ -1,10 +1,10 @@
 (function() {
 	
 	//Defining the base architecture based on the waf-core/widget class
-    var widget = WAF.require('waf-core/widget');
+    	var widget = WAF.require('waf-core/widget');
  
  	// Creating the Basic Container widget 
-    var BasicContainer = widget.create('BasicContainer');
+    	var BasicContainer = widget.create('BasicContainer');
 	
 	// Adding the behavior layout container, which grants to the widget all the methods available in waf-behavior/layout/container 
 	BasicContainer.inherit(WAF.require('waf-behavior/layout/container')); 
