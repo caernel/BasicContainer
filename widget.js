31567de6df1847f9118105231746d8ab1717cdd2
Ricardo <ricardo.mello@4d.com>
2013-12-04 18:45:04 +0100

    Changes from last wakanda build

--- widget.js
+++ widget.js
@@ -1,13 +1,11 @@
-(function() {
+WAF.define('BasicContainer', function() {
 	
-	//Defining the base architecture based on the waf-core/widget class
-    var widget = WAF.require('waf-core/widget');
- 
- 	// Creating the Basic Container widget 
-    var BasicContainer = widget.create('BasicContainer');
-	
-	// Adding the behavior layout container, which grants to the widget all the methods available in waf-behavior/layout/container 
-	BasicContainer.inherit(WAF.require('waf-behavior/layout/container')); 
-	
-	
-})();
+    var Widget = WAF.require('waf-core/widget'),
+    
+    BasicContainer = Widget.create('BasicContainer');
+
+    BasicContainer.inherit(WAF.require('waf-behavior/layout/container')); 
+    
+    return BasicContainer;
+
+});
